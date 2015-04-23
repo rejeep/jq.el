93dcf99fd3ac14fd54c9b541aa56ab5269c703db
Johan Andersson <johan.rejeep@gmail.com>
2015-04-23 09:14:48 +0200

    Add jq.el with boilerplate

---
+++ jq.el
@@ -0,0 +1,51 @@
+;;; jq.el --- TODO
+
+;; Copyright (C) 2015 Johan Andersson
+
+;; Author: Johan Andersson <johan.rejeep@gmail.com>
+;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
+;; Version: 0.0.1
+;; Keywords: speed, convenience
+;; URL: http://github.com/rejeep/jq.el
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;;; Code:
+
+(defvar jq-mode-hook nil
+  "Mode hook for `jq-mode'.")
+
+(defvar jq-mode-map (make-sparse-keymap)
+  "Keymap for jq-mode.")
+
+;;;###autoload
+(define-derived-mode jq-mode special-mode "JQ"
+  ""
+  (kill-all-local-variables)
+  (setq mode-name "JQ")
+  (setq major-mode 'jq-mode)
+  (use-local-map jq-mode-map)
+  (run-mode-hooks 'jq-mode-hook))
+
+(provide 'jq)
+
+;;; jq.el ends here
